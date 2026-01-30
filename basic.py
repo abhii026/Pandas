d10a9733dc80551e8ef81e79786bbbc67c77bd43
Abhishek Singh <iabhii026@gmail.com>
2026-01-30 20:30:17 +0530

    Added basic of pandas

---
+++ basic.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+#read data from CSV file into a dataframe
+df=pd.read_csv(r"C:\Users\iabhi\OneDrive\Desktop\DataScience_Python\datasets\diabetes.csv")
+print(df)
+
+#read excel file
+# df=pd.read_excel()
+
+#read json file
+# df=pd.read_json()
